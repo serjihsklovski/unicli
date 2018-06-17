4200e5bbacd193c08bbe091adb66962fe4ef38cd
Sergey Sokolov <serjihsklovski@gmail.com>
2018-06-17 13:25:00 +0300

    New algorithm for `ClassProviderImpl`. The bug is fixed

--- src/main/java/com/serjihsklovski/unicli/util/classprovider/ClassProviderImpl.java
+++ src/main/java/com/serjihsklovski/unicli/util/classprovider/ClassProviderImpl.java
@@ -1,17 +1,24 @@
 package com.serjihsklovski.unicli.util.classprovider;
 
-import java.io.File;
+import com.serjihsklovski.unicli.util.classprovider.extractclassnamepolicy.ExtractClassNamePolicy;
+import com.serjihsklovski.unicli.util.classprovider.extractclassnamepolicy.FileSchemeExtractClassNamePolicy;
+import com.serjihsklovski.unicli.util.classprovider.extractclassnamepolicy.JarSchemeExtractClassNamePolicy;
+import com.serjihsklovski.unicli.util.classprovider.findresourcespolicy.FileSchemeFindResourcesPolicy;
+import com.serjihsklovski.unicli.util.classprovider.findresourcespolicy.FindResourcesPolicy;
+import com.serjihsklovski.unicli.util.classprovider.findresourcespolicy.JarSchemeFindResourcesPolicy;
+
 import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.Files;
+import java.net.URLClassLoader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Collection;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -20,9 +27,21 @@ import java.util.stream.Stream;
 
 public class ClassProviderImpl implements ClassProvider {
 
-    private static final Boolean FILES = false;
+    private static final String SCHEME_JAR = "jar";
 
-    private static final Boolean DIRS = true;
+    private static final String SCHEME_FILE = "file";
+
+    private static final Set<String> EXCLUDED_PACKAGES = new HashSet<>();
+
+    private static final String GROUP_FILE_PATH = "filePath";
+
+    private static final String TEMPLATE_FILE_PATH_URI = String.format("^file:/*(?<%s>.*)", GROUP_FILE_PATH);
+
+    private static final String TEMPLATE_CLASS_FILE = ".*\\.class$";
+
+    static {
+        EXCLUDED_PACKAGES.add("com.sun.");
+    }
 
     @Override
     public Set<Class> fetchAllClassesByRoots(Set<String> roots) {
@@ -32,90 +51,143 @@ public class ClassProviderImpl implements ClassProvider {
                 .collect(Collectors.toSet());
     }
 
-    private Stream<Class> findClassesByPackageName(String packageName) {
+    private Stream<? extends Class<?>> findClassesByPackageName(String packageName) {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        Enumeration<URL> resources = getResources(classLoader, packageName);
-        Collection<String> directories = getDirectories(resources);
-        return getClasses(directories, packageName);
+        Set<URI> resourcesRoots = getResourcesRoots(classLoader, packageName);
+        Set<URI> classPaths = getClassPaths(classLoader);
+        Map<URI, Set<URI>> classPathToResources = groupResourcesByClassPath(resourcesRoots, classPaths);
+
+        return classPathToResources.entrySet().stream()
+                .map(entry -> getClassesCanonicalNames(entry.getKey(), entry.getValue()))
+                .flatMap(classesCanonicalNames -> classesCanonicalNames)
+                .filter(this::notExcludedClass)
+                .filter(this::notInnerClass)
+                .distinct()
+                .map(this::findClassForName)
+                .filter(Objects::nonNull);
     }
 
-    private static Enumeration<URL> getResources(ClassLoader classLoader, String packageName) {
-        String packageNamePath = packageName.replace('.', '/');
+    private Set<URI> getResourcesRoots(ClassLoader classLoader, String packageName) {
+        String packageNamePath = packageName.replace(ExtractClassNamePolicy.CLASS_SEPARATOR,
+                ExtractClassNamePolicy.URI_SEPARATOR);
+
+        Enumeration<URL> resources;
         try {
-            return classLoader.getResources(packageNamePath);
+            resources = classLoader.getResources(packageNamePath);
         } catch (IOException ioe) {
             throw new RuntimeException(ioe);
         }
-    }
 
-    private Collection<String> getDirectories(Enumeration<URL> resources) {
-        Collection<String> directories = new HashSet<>();
+        Set<URI> resourcesRoots = new HashSet<>();
         while (resources.hasMoreElements()) {
-            try {
-                directories.add(Paths.get(resources.nextElement().toURI()).toString());
-            } catch (URISyntaxException use) {
-                throw new RuntimeException(use);
-            }
+            resourcesRoots.add(convertUrlToUri(resources.nextElement()));
         }
-        return directories;
+        return resourcesRoots;
     }
 
-    private Stream<Class> getClasses(Collection<String> directories, String packageName) {
-        return directories.stream()
-                .map(dir -> Paths.get(dir))
-                .filter(dir -> Files.exists(dir))
-                .map(this::findFilesRecursively)
-                .flatMap(filesStream -> filesStream)
-                .filter(this::isClassFile)
-                .map(classFilePath -> trimToClassName(classFilePath, packageName))
-                .map(this::findClassForName);
+    private Set<URI> getClassPaths(ClassLoader classLoader) {
+        return Stream.of(((URLClassLoader) classLoader).getURLs())
+                .map(this::convertUrlToUri)
+                .map(Paths::get)
+                .map(Path::toUri)
+                .map(URI::normalize)
+                .collect(Collectors.toSet());
     }
 
-    private Stream<Path> findFilesRecursively(Path path) {
-        if (Files.isDirectory(path)) {
-            try {
-                Map<Boolean, List<Path>> filesAndDirs = Files.list(path)
-                        .collect(Collectors.groupingBy(p -> Files.isDirectory(p)));
+    private Map<URI, Set<URI>> groupResourcesByClassPath(Set<URI> resourcesRoots, Set<URI> submittedClassPaths) {
+        Map<URI, Set<URI>> classPathToResources = new HashMap<>();
+        resourcesRoots.stream()
+                .map(this::getAllResources)
+                .forEach(resourcesGroup -> {
+                    if (resourcesGroup.size() == 0) {
+                        throw new RuntimeException("No resources found for the class path.");
+                    }
+                    URI typicalGroupUri = resourcesGroup.stream().findFirst().get();
+                    URI classPath = getResourceClassPath(typicalGroupUri, submittedClassPaths);
+                    classPathToResources.put(classPath, resourcesGroup);
+                });
 
-                if (filesAndDirs.containsKey(FILES) && filesAndDirs.containsKey(DIRS)) {
-                    return Stream.concat(
-                            filesAndDirs.get(FILES).stream(),
-                            filesAndDirs.get(DIRS).stream()
-                                    .map(this::findFilesRecursively)
-                                    .flatMap(files -> files)
-                    );
-                } else if (filesAndDirs.containsKey(FILES)) {
-                    return filesAndDirs.get(FILES).stream();
-                } else if (filesAndDirs.containsKey(DIRS)) {
-                    return filesAndDirs.get(DIRS).stream()
-                            .map(this::findFilesRecursively)
-                            .flatMap(files -> files);
-                } else {
-                    return Stream.empty();
-                }
+        return classPathToResources;
+    }
 
-            } catch (IOException ioe) {
-                throw new RuntimeException(ioe);
-            }
+    private URI getResourceClassPath(URI resourceUri, Set<URI> submittedClassPaths) {
+        if (resourceUri.getScheme().equals(SCHEME_JAR)) {
+            // emulate `file` scheme behaviour
+            JarSchemeUriExtractor uriExtractor = JarSchemeUriExtractor.extract(resourceUri);
+            resourceUri = URI.create(String.format("%s%s%s", uriExtractor.getJarFileUri().toString(),
+                    ExtractClassNamePolicy.URI_SEPARATOR, uriExtractor.getJarEntryName()));
         }
 
-        return Stream.of(path);
+        return findResourceClassPath(resourceUri, submittedClassPaths);
     }
 
-    private boolean isClassFile(Path path) {
-        return path.toString().endsWith(".class");
+    private URI findResourceClassPath(URI resourcePath, Set<URI> submittedClassPaths) {
+        String resourcePathString = getFilePath(resourcePath);
+        return submittedClassPaths.stream()
+                .filter(classPath -> resourcePathString.startsWith(getFilePath(classPath)))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException(String.format("Cannot find classpath for the resource `%s`.",
+                        resourcePath)));
     }
 
-    private String trimToClassName(Path classFilePath, String packageName) {
-        String packageNamePath = packageName.replace(".", "\\" + File.separator);
-        Pattern packagePattern = Pattern.compile(String.format("(.*)(%s.*)\\.class", packageNamePath));
-        Matcher matcher = packagePattern.matcher(classFilePath.toString());
-
-        final int CLASS_NAME_GROUP = 2;
-        if (matcher.matches()) {
-            return matcher.group(CLASS_NAME_GROUP).replace(File.separator, ".");
+    private String getFilePath(URI filePathUri) {
+        Pattern pattern = Pattern.compile(TEMPLATE_FILE_PATH_URI);
+        Matcher matcher = pattern.matcher(filePathUri.toString());
+        if (!matcher.matches()) {
+            throw new RuntimeException("The `filePathUri` does not match to the file path template.");
         }
-        throw new RuntimeException(String.format("Cannot find a package name `%s`", packageName));
+        return matcher.group(GROUP_FILE_PATH);
+    }
+
+    private URI convertUrlToUri(URL url) {
+        try {
+            return url.toURI();
+        } catch (URISyntaxException use) {
+            throw new RuntimeException(use);
+        }
+    }
+
+    private Set<URI> getAllResources(URI resourcesRoot) {
+        return getFindResourcesPolicy(resourcesRoot.getScheme()).findResources(resourcesRoot);
+    }
+
+    private Stream<String> getClassesCanonicalNames(URI classPath, Set<URI> resources) {
+        URI typicalGroupUri = resources.stream().findFirst().orElseThrow(() -> new RuntimeException(String.format(
+                "Unexpected error occurred: no resources found for the class path `%s`.", classPath)));
+
+        ExtractClassNamePolicy extractClassNamePolicy = getExtractClassNamePolicy(typicalGroupUri.getScheme());
+
+        return resources.stream()
+                .filter(resource -> Pattern.compile(TEMPLATE_CLASS_FILE).asPredicate().test(resource.toString()))
+                .map(resource -> extractClassNamePolicy.extractClassName(classPath, resource));
+    }
+
+    private FindResourcesPolicy getFindResourcesPolicy(String uriScheme) {
+        switch (uriScheme) {
+            case SCHEME_JAR:
+                return new JarSchemeFindResourcesPolicy();
+
+            case SCHEME_FILE:
+                return new FileSchemeFindResourcesPolicy();
+
+            default:
+                throw new RuntimeException(String.format(
+                        "Cannot find `FindResourcesPolicy` implementation for `%s` URI scheme.", uriScheme));
+        }
+    }
+
+    private ExtractClassNamePolicy getExtractClassNamePolicy(String uriScheme) {
+        switch (uriScheme) {
+            case SCHEME_JAR:
+                return new JarSchemeExtractClassNamePolicy();
+
+            case SCHEME_FILE:
+                return new FileSchemeExtractClassNamePolicy();
+
+            default:
+                throw new RuntimeException(String.format(
+                        "Cannot find `ExtractClassNamePolicy` implementation for `%s` URI scheme.", uriScheme));
+        }
     }
 
     private Class<?> findClassForName(String className) {
@@ -123,7 +195,18 @@ public class ClassProviderImpl implements ClassProvider {
             return Class.forName(className);
         } catch (ClassNotFoundException cnfe) {
             throw new RuntimeException(cnfe);
+        } catch (NoClassDefFoundError ncdfe) {
+            return null;    // filter nullable results in the top method
         }
+    }
+
+    private boolean notInnerClass(String className) {
+        return !className.contains("$");
+    }
+
+    private boolean notExcludedClass(String className) {
+        return EXCLUDED_PACKAGES.stream()
+                .noneMatch(className::startsWith);
     }
 
 }
